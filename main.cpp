5bc4dd80a5f777e6ddeee4ba4f1735c0ef580385
Hugo PERINAZZO <Hugoperinazzopwolfdev.net>
2018-07-21 13:35:26 +0200

    Comit1

--- main.cpp
+++ main.cpp
@@ -1,4 +1,27 @@
 #include <iostream> // For debug purposes
+#include <tuple>
+#include <vector>
+#include <string>
+#include <ctime>
+
+std::vector<std::tuple<int, std::string, std::string, bool>> members_table;  // on stock id, nom, prénom, state(banni,préivilégié)
+std::vector<std::tuple<std::string, int, int, float, std::time_t>> books_table; // on stock titre, state(empreinté,achat,perdu,dispo),prix,date de retour en cas d'empreint
+
+struct members
+{
+std::tuple<int, std::string, std::string, bool> membres_tuple;
+}
+
+
+
+
+
+
+
+
+
+
+
 
 int main(int, char**){
 
