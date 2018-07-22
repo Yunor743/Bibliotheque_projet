64400c43c5b8c412f62f97553e888d28b4d30295
Hugo PERINAZZO <Hugoperinazzopwolfdev.net>
2018-07-22 22:29:45 +0200

    V.22/07/2018

--- content/main.cpp
+++ content/main.cpp
@@ -1,5 +1,6 @@
 /*books.hpp*/
-#pragma once
+
+//#pragma once
 #include <unordered_map>
 #include <string>
 #include <ctime>
@@ -20,8 +21,9 @@ struct BookInfo // on stock toute les informations relatives à un livre
     BookState state;            //état du livre (empreinté, perdu, en achat, dispo)
     int id_member;              //id du membre qui à empreinté le livre
     float price;                //prix théorique du livre si il doit y avoir un remboursement
-    std::time_t return_date;    //Date où le livre doit être returner à la bibliothèque
+    std::time_t return_date;    //Date où le livre doit être retourner à la bibliothèque
 };
+
 struct Books
 {
   protected:                                      //On crée notre générateur, il sera protégé, donc appelable seulement dans cette classe et les classe enfant
@@ -46,7 +48,16 @@ struct Books
 
 
 
+
+
+
 /*members.hpp*/
+
+//#pragma once
+//#include <unordered_map>
+//#include <string>
+//#include <ctime>
+//#include <random>
 
 using uint = unsigned int;  //on poura utiliser uint pour représenter un unsigned int
 
@@ -68,7 +79,7 @@ struct Members //contient une table de tous les membres ainsi que des fonctions 
     std::mt19937 generator;
   public:                                         //On retourne au public, les éléments seront à nouveau appelables en dehors de l'instance de la classe
     std::unordered_map<uint, MemberInfo> table;   // table de tout les membres
-    void insert(std::string nom, std::string prenom, MemberState state = MemberState::NORMAL); //on déclare la fonction membre qui permet d'insérer une nouvelle entrée dans la table
+    void insert(std::string nom, std::string prenom, MemberState state); //on déclare la fonction membre qui permet d'insérer une nouvelle entrée dans la table
     void disp() const;
 };
 
@@ -85,16 +96,11 @@ struct Members //contient une table de tous les membres ainsi que des fonctions 
 
 
 
+
+
 /*books.cpp*/
 
-struct BookInfo // on stock toute les informations relatives à un livre
-{
-    std::string title;          //titre du livre
-    int state;                  //état du livre (empreinté, perdu, en achat, dispo)
-    int id_member;              //id du membre qui à empreinté le livre
-    float price;                //prix théorique du livre si il doit y avoir un remboursement
-    std::time_t return_date;    //Date où le livre doit être returner à la bibliothèque
-};
+
 void Books::insert()
 {
 
@@ -103,6 +109,7 @@ void Books::disp() const
 {
 
 }
+
 
 
 
@@ -128,9 +135,8 @@ void Members::insert(std::string nom, std::string prenom, MemberState state = Me
     key = generator();
   }
   while (table.find(key) == table.end()); // Si table.find(key) est égale à table.end (donc la fin de table) alors c'est que cette clé n'éxiste pas encore et on peut passer à la suite, sinon on recommence
-  table.emplace(key, MemberInfo{nom, prenom, MemberState::NORMAL, std::chrono::system_clock::to_time_t(std::chrono::system_clock::now())}); //
+  table.emplace(key, MemberInfo{nom, prenom, state, std::chrono::system_clock::to_time_t(std::chrono::system_clock::now())}); //
 }
-
 void Members::disp() const
 {
 
