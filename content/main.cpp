392d891fc16a2d1d098ed8f1eafb2fae35526f93
Hugo PERINAZZO <Hugoperinazzopwolfdev.net>
2018-07-24 22:54:10 +0200

    comit 24/07

--- content/main.cpp
+++ content/main.cpp
@@ -1,3 +1,39 @@
+/*classic_content.hpp*/
+
+//#pragma once
+#include <iostream>
+#include <ctime>
+#include <iomanip>
+
+using uint = unsigned int; //on pourra utiliser uint pour représenter un unsigned int
+enum class DateDivision : uint
+{
+    YEAR,
+    MONTH,
+    DAY,
+    HOUR,
+    MIN,
+    SEC
+};
+int get_date_spec(DateDivision date_div);
+std::time_t parse_days_in_date(uint nb_of_days, std::time_t start_date);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 /*books.hpp*/
 
 //#pragma once
@@ -19,7 +55,7 @@ struct BookInfo // on stock toute les informations relatives à un livre
 {
     std::string title;          //titre du livre
     BookState state;            //état du livre (empreinté, perdu, en achat, dispo)
-    uint id_member;              //id du membre qui à empreinté le livre
+    uint id_member;             //id du membre qui à empreinté le livre
     float price;                //prix théorique du livre si il doit y avoir un remboursement
     std::time_t return_date;    //Date où le livre doit être retourner à la bibliothèque
 };
@@ -50,6 +86,9 @@ struct Books
 
 
 
+
+
+
 /*members.hpp*/
 
 //#pragma once
@@ -57,7 +96,7 @@ struct Books
 //#include <string>
 //#include <ctime>
 //#include <random>
-#include <chrono>
+//#include <chrono>
 
 //using uint = unsigned int;  //on poura utiliser uint pour représenter un unsigned int
 
@@ -94,16 +133,68 @@ struct Members //contient une table de tous les membres ainsi que des fonctions 
 
 
 
+
+
+
 /*system.hpp*/
 
 //using uint = unsigned int;  //on poura utiliser uint pour représenter un unsigned int
 
 struct System
 {
-  void borrow(Books book_inst, uint book_id, uint member_id);
+  void borrow(Books book_inst, uint book_id, uint member_id, uint days_of_borrowing);
   void return_book();
 };
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*classic_content.cpp*/
+int get_date_spec(DateDivision date_div = DateDivision::YEAR)
+{
+    std::time_t result = std::time(nullptr);
+    tm full_date = *std::localtime(&result);
+    switch(date_div)
+    {
+        case DateDivision::YEAR:
+            return full_date.tm_year + 1900; //Les années commence à se compter à partir de l'année 1900
+            break;
+        case DateDivision::MONTH:
+            return full_date.tm_mon + 1; //les mois se compte à partir du moi 0
+            break;
+        case DateDivision::DAY:
+            return full_date.tm_mday; //Les jours
+            break;
+        case DateDivision::HOUR:
+            return full_date.tm_hour; // les heures
+            break;
+        case DateDivision::MIN:
+            return full_date.tm_min; // les minutes
+            break;
+        case DateDivision::SEC:
+            return full_date.tm_sec; // les secondes
+            break;
+    }
+    return 1;
+}
+std::time_t parse_days_in_date(uint nb_of_days, std::time_t start_date = std::time(nullptr))
+{
+
+}
 
 
 
@@ -154,6 +245,8 @@ void Books::disp() const
 
 
 
+
+
 /*members.cpp*/
 
 void Members::insert(std::string nom, std::string prenom, MemberState state = MemberState::NORMAL) //on défini la fonction membre qui permet d'insérer une nouvelle ligne dans la table
@@ -178,17 +271,24 @@ void Members::disp() const
 
 
 
+
+
+
+
+
+
 /*system.cpp*/
 
-void System::borrow(Books book_inst, uint book_id, uint member_id) //function permettant l'emprunt d'un livre
+void System::borrow(Books book_inst, uint book_id, uint member_id, uint days_of_borrowing = 30) //function permettant l'emprunt d'un livre
 {
-  if(book_inst.table.find(book_id) != book_inst.table.end()) //on s'assure que le livre demandé existe
+  if(book_inst.table.find(book_id) != book_inst.table.end())            //On s'assure que le livre demandé existe
   {
-    BookInfo one_bookinfo = book_inst.table[book_id]; //on instancie toute les informations sur ce livre dans one_bookinfo
-    if(one_bookinfo.state == BookState::AVAILABLE) //on s'assure que le livre est disponible
+    BookInfo one_bookinfo = book_inst.table[book_id];                   //on instancie toute les informations sur ce livre dans one_bookinfo
+    if(one_bookinfo.state == BookState::AVAILABLE)                      //on s'assure que le livre est disponible
     {
-      one_bookinfo.state = BookState::BORROWED;
-      one_bookinfo.id_member = member_id;
+      one_bookinfo.state = BookState::BORROWED;                         //On déclare le livre comme empreinté
+      one_bookinfo.id_member = member_id;                               //On défini qui est le dernier empreinteur
+      one_bookinfo.return_date = parse_days_in_date(days_of_borrowing); //On défini la date de retour
     }
     else
     {
@@ -200,6 +300,7 @@ void System::borrow(Books book_inst, uint book_id, uint member_id) //function pe
     //Erreur: le livre à empreinter n'éxiste pas
   }
 }
+
 
 
 
