6bd1b1aac76d3f66932a1543a902dd3a4779288a
Yunor <>
2018-08-12 09:11:01 +0200

    We try to install wingw and cmake

---
+++ build/CMakeFiles/irondel.dir/DependInfo.cmake
@@ -0,0 +1,32 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  "RC"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "E:/code/irondel_project/content/books.cpp" "E:/code/irondel_project/build/CMakeFiles/irondel.dir/content/books.cpp.obj"
+  "E:/code/irondel_project/content/classic_content.cpp" "E:/code/irondel_project/build/CMakeFiles/irondel.dir/content/classic_content.cpp.obj"
+  "E:/code/irondel_project/content/main.cpp" "E:/code/irondel_project/build/CMakeFiles/irondel.dir/content/main.cpp.obj"
+  "E:/code/irondel_project/content/members.cpp" "E:/code/irondel_project/build/CMakeFiles/irondel.dir/content/members.cpp.obj"
+  "E:/code/irondel_project/content/system.cpp" "E:/code/irondel_project/build/CMakeFiles/irondel.dir/content/system.cpp.obj"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+set(CMAKE_DEPENDS_CHECK_RC
+  "E:/code/irondel_project/ressources/icon.rc" "E:/code/irondel_project/build/CMakeFiles/irondel.dir/ressources/icon.rc.obj"
+  )
+
+# The include file search paths:
+set(CMAKE_RC_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
